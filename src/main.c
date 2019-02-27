e72b681e8b1f1a5f02e550b2754c83bf5fc83669
hiddendoom45 <hiddendoom45@gmail.com>
2019-02-26 19:04:15 -0500

    Buffered reads
    -undefined beahviour is the best, takes ages to figure that out

--- src/main.c
+++ src/main.c
@@ -1,24 +1,77 @@
 #include <stdio.h>
 
+#define BUFSIZE 4096
+
+int readachar(FILE *f, size_t *read, size_t *index, unsigned char *p){
+	if(*read==*index){
+		if(feof(f)) return -1;
+		*read = fread(p,sizeof(unsigned char), BUFSIZE,f);
+		*index = 0;
+		if(ferror(f)) return -1;
+		return 0;
+	}
+	(*index)++;
+	return 0;
+}
+
 int main(int argc, char *argv[]) {
 	if(argc==3){
 		FILE *a = fopen(argv[1],"rb");
 		FILE *b = fopen(argv[2],"rb");
-		int c, d;
 		long i;
-		for (i = 0; (c=fgetc(a))==(d=fgetc(b))&&c!=EOF;i++);
-		if(ferror(a)!=0||ferror(b)!=0){
+		size_t cr = 0, dr = 0, ci = 0, di = 0;
+		unsigned char *c, *d;
+		c = malloc(BUFSIZE * sizeof(unsigned char));
+		d = malloc(BUFSIZE * sizeof(unsigned char));
+		*c = 0;
+		*d = 0;
+		int err = 0;
+		for( i = 0; ci==cr||di==dr||*(d+di)==*(c+ci); i++, ci++, di++){
+			if(ci == cr) {
+				if(feof(a)){
+					printf("eof");
+					break;
+				} 
+				cr = fread(c, sizeof(unsigned char), BUFSIZE,a);
+				if(cr==0){
+					//to ensure other file also reads EOF
+					dr = fread(d, sizeof(unsigned char), BUFSIZE,b);
+					break;
+				}
+				ci = 0;
+				if(ferror(a)){
+					printf("err");
+					err=1;
+					break;
+				}
+			}
+			if(di == dr){
+				if(feof(b)){
+					printf("eof");
+					break;
+				} 
+				dr = fread(d, sizeof(unsigned char), BUFSIZE,b);
+				if(dr==0)break;
+				di = 0;
+				if(ferror(b)){
+					printf("err");
+					err=1;
+					break;
+				}
+			}
+		}
+		if(err){
 			printf("error %d,%d",ferror(a),ferror(b));
 		}
-		if(c==EOF&&d==EOF){
-			printf("Files are the same length=0x%lx\n",i);
+		if(feof(a)==feof(b)!=0&&*(d+di)==*(c+ci)){
+			printf("files are the same 0x%lx\n",i);
 		}
 		else{
-			printf("First difference at 0x%lx 0x%x!=0x%x\n",i,c,d);
+			printf("First difference at 0x%lx 0x%02x!=0x%02x\n",i,*(c+ci),*(d+di));
 			printf(">>>%s\n",argv[1]);
-			for(i = 0;i<16&&(c=fgetc(a))!=EOF;i++) printf("%02x ",c);
+			for(i = 0;i<16&&readachar(a,&cr,&ci,c)!=-1;i++) printf("%02x ",*(c+ci));
 			printf("\n>>>%s\n",argv[2]);
-			for(i = 0;i<16&&(c=fgetc(b))!=EOF;i++) printf("%02x ",c);
+			for(i = 0;i<16&&readachar(b,&dr,&di,d)!=-1;i++) printf("%02x ",*(d+di));
 			printf("\n");
 		}
 	}
